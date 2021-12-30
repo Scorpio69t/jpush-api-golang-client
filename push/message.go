5d0f64a4020a8ac3c7c81717d43ed7b0de5f33a1
瑞涛 杨 <yangruitao@cd-hst.com>
2021-12-30 18:22:07 +0800

    Message 添加注释

--- push/message.go
+++ push/message.go
@@ -7,22 +7,27 @@ type Message struct {
 	Extras      map[string]interface{} `json:"extras,omitempty"`       // JSON 格式的可选参数
 }
 
+// SetContent 设置消息内容
 func (m *Message) SetContent(content string) {
 	m.MsgContent = content
 }
 
+// SetTitle 设置消息标题
 func (m *Message) SetTitle(title string) {
 	m.Title = title
 }
 
+// SetContentType 设置消息内容类型
 func (m *Message) SetContentType(contentType string) {
 	m.ContentType = contentType
 }
 
+// SetExtras 设置消息扩展内容
 func (m *Message) SetExtras(extras map[string]interface{}) {
 	m.Extras = extras
 }
 
+// AddExtras 添加消息扩展内容
 func (m *Message) AddExtras(key string, value interface{}) {
 	if m.Extras == nil {
 		m.Extras = make(map[string]interface{})
