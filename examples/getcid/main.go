c60638496af9368f0ced90eba4acbe3ab6f898a9
瑞涛 杨 <yangruitao@cd-hst.com>
2021-12-31 17:34:58 +0800

    更新例子

--- examples/getcid/main.go
+++ examples/getcid/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	cid := jpush.NewCidRequest(1, "")
-	res, err := cid.GetCidList("xxx", "xxx") // 这里的 key 和 secret 需要替换成自己的
+	res, err := cid.GetCidList("2c741f8a5cee16580357e791", "860e76cd5ccb91f313463d4f") // 这里的 key 和 secret 需要替换成自己的
 	if err != nil {
 		fmt.Println(err)
 	} else {
---
+++ examples/push/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Scorpio69t/jpush-api-golang-client"
+)
+
+func main() {
+	// Platform: all
+	var pf jpush.Platform
+	pf.Add(jpush.ANDROID)
+	pf.Add(jpush.IOS)
+	pf.Add(jpush.WINPHONE)
+	// pf.All()
+
+	// Audience: tag
+	var at jpush.Audience
+	s := []string{"tag1", "tag2"}
+	at.SetTag(s)
+	id := []string{"1", "2"}
+	at.SetID(id)
+	// at.All()
+
+	// Notification
+	var n jpush.Notification
+	n.SetAlert("alert")
+	n.SetAndroid(&jpush.AndroidNotification{Alert: "alert", Title: "title"})
+	n.SetIos(&jpush.IosNotification{Alert: "alert", Badge: 1})
+	n.SetWinPhone(&jpush.WinPhoneNotification{Alert: "alert"})
+
+	// Message
+	var m jpush.Message
+	m.MsgContent = "This is a message"
+	m.Title = "Hello"
+
+	// PayLoad
+	payload := jpush.NewPayLoad()
+	payload.SetPlatform(&pf)
+	payload.SetAudience(&at)
+	payload.SetNotification(&n)
+	payload.SetMessage(&m)
+
+	// Send
+	c := jpush.NewJPushClient("appKey", "masterSecret") // appKey and masterSecret can be gotten from https://www.jiguang.cn/
+	data, err := payload.Bytes()
+	if err != nil {
+		panic(err)
+	}
+	res, err := c.Push(data)
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+	} else {
+		fmt.Printf("ok: %v\n", res)
+	}
+}
