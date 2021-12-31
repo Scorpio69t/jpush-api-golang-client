7a49392ee7120046ec690a4f2971131e40c436e2
瑞涛 杨 <yangruitao@cd-hst.com>
2021-12-31 10:21:22 +0800

    添加 SmsMessage

--- push/message.go
+++ push/message.go
@@ -12,6 +12,14 @@ type InappMessage struct {
 	InAppMessage bool `json:"inapp_message"`
 }
 
+type SmsMessage struct {
+	DelayTime    int         `json:"delay_time"`          // 单位为秒，不能超过24小时。设置为0，表示立即发送短信。该参数仅对 android 和 iOS 平台有效，Winphone 平台则会立即发送短信。
+	Signid       int         `json:"signid,omitempty"`    // 签名ID，该字段为空则使用应用默认签名。
+	TempId       int64       `json:"temp_id,omitempty"`   // 短信补充的内容模板 ID。没有填写该字段即表示不使用短信补充功能。
+	TempPara     interface{} `json:"temp_para,omitempty"` // 短信模板中的参数。
+	ActiveFilter bool        `json:"active_filter"`       // active_filter 字段用来控制是否对补发短信的用户进行活跃过滤，默认为 true ，做活跃过滤；为 false，则不做活跃过滤；
+}
+
 // SetContent 设置消息内容
 func (m *Message) SetContent(content string) {
 	m.MsgContent = content
@@ -43,4 +51,29 @@ func (m *Message) AddExtras(key string, value interface{}) {
 // SetInAppMessage 设置是否启用应用内消息
 func (i *InappMessage) SetInAppMessage(inAppMessage bool) {
 	i.InAppMessage = inAppMessage
+}
+
+// SetDelayTime 设置短信发送延时时间
+func (s *SmsMessage) SetDelayTime(delayTime int) {
+	s.DelayTime = delayTime
+}
+
+// SetSignid 设置短信签名ID
+func (s *SmsMessage) SetSignid(signid int) {
+	s.Signid = signid
+}
+
+// SetTempId 设置短信模板ID
+func (s *SmsMessage) SetTempId(tempId int64) {
+	s.TempId = tempId
+}
+
+// SetTempPara 设置短信模板参数
+func (s *SmsMessage) SetTempPara(tempPara interface{}) {
+	s.TempPara = tempPara
+}
+
+// SetActiveFilter 设置是否对补发短信的用户进行活跃过滤
+func (s *SmsMessage) SetActiveFilter(activeFilter bool) {
+	s.ActiveFilter = activeFilter
 }
